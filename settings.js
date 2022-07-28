e1fac2f0c148635d5e95b04338796cdf713e3644
hoseinbaloch1 <110195595+hoseinbaloch1@users.noreply.github.com>
2022-07-28 23:18:43 +0430

    Update settings.js

--- settings.js
+++ settings.js
@@ -61,16 +61,16 @@ global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.owner = ['9989339658384]
+global.ownertag = '989339658384' //ur tag number
+global.botname = 'hosein bot mdv4' //ur bot name
+global.linkz = "https:// //your theme url which will be displayed on whatsapp
+global.websitex = "https://" //ur website to be displayed
+جهانی . botscript  =  'https://github.com/Hoseinbaloch1/CheemsBot-MD4'  //لینک اسکریپتo
+جهانی . reactmoji  =  "🕊" //ur menu react emoji
+global.themeemoji = "🐼" //ur theme emoji
+global.packname = "𝐇𝐎𝐒𝐄𝐈𝐍 𝐁𝐀𝐋𝐎𝐂𝐇" //ur sticker watermark packname
+global.author = "𝐇𝐎𝐒𝐄𝐈𝐍 𝐁𝐀𝐋𝐎𝐂𝐇" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['989339658384'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
